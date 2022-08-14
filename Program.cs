8f4c92c993d09e265085c1efd0a1a2fb0250dccd
Svetlana <lana.chebotareva.95@mail.ru>
2022-08-14 14:32:59 +0400

    Задача 13

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+(Строки и циклы в решении использовать нельзя!)*/
+
+Console.Write("Задача  13");
+Console.Write("Введите число K ");
+int pp = int.Parse(Console.ReadLine());
+double s= pp / 100;
+Console.WriteLine(s);
+if (s == 0){
+ 
+  Console.WriteLine("-> третьей цифры нет");
+}
+if(s>100){
+    s = s %10;
+    Console.WriteLine("третья цифра -> " + s); 
+}
